1afc73c2884102ffd23836340f9e08e4b67b2a48
BhangaleGunjan <120701549+BhangaleGunjan@users.noreply.github.com>
2025-04-13 14:07:59 +0530

    Delete modules/test_recognition.py

--- modules/test_recognition.py
+++
@@ -1,5 +0,0 @@
-from face_recognition_handler import FaceRecognizer
-
-recognizer = FaceRecognizer()
-name = recognizer.recognize_face()
-print(f"Hello, {name}!" if name != "Unknown" else "Face not recognized.")
